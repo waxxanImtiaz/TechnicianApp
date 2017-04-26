0cdedf6eeb66df2f15a0ace2f0fc467ef86f91cf
unknown <wassanimtiaz@outlook.com>
2017-04-26 17:29:31 +0500

    setting

--- app/src/main/java/com/example/eureka/technicianapp/ServiceVisitUpdate.java
+++ app/src/main/java/com/example/eureka/technicianapp/ServiceVisitUpdate.java
@@ -7,12 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class ServiceVisitUpdate extends Fragment {
 
+    private Spinner spItemCategory;
+    private Spinner spStatus;
+    private String[] itemCategoryList;
+    private String[] statusList;
     public ServiceVisitUpdate() {
         // Required empty public constructor
+        itemCategoryList = new String[]{"Refrigerator","AC","Deep Freezer","Micro wave","T.V","DVD","Camera","Mobile Phone"
+        ,"Washing Machine","Water Dispenser"};
+        statusList = new String[]{"Repaired","Not Repaired","Rejected Quotation","Visit Again to repair","Send to workshop"};
     }
 
     @Override
@@ -26,6 +35,16 @@ public class ServiceVisitUpdate extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_service_visit_update, container, false);
+    }
+
+    public void init(View v){
+        ArrayAdapter<String> itemCatAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item,itemCategoryList);
+        spItemCategory = (Spinner)v.findViewById(R.id.sp_item_category);
+        spItemCategory.setAdapter(itemCatAdapter);
+        spStatus = (Spinner)v.findViewById(R.id.sp_status);
+        ArrayAdapter<String> statusAdapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item,statusList);
+        spStatus.setAdapter(statusAdapter);
+
     }
 
 }
